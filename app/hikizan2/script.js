0f31cc29c1959bfedf716a339abb604f4d697de6
naohiko560 <private.mail.nao@gmail.com>
2024-07-07 19:55:21 +0900

    ひきざん_ふつう：スタート音・点数表示追加

--- app/hikizan2/script.js
+++ app/hikizan2/script.js
@@ -1,20 +1,36 @@
 // 生成する問題文の数字
-let num1 = null;
-let num2 = null;
+let num1 = 0;
+let num2 = 0;
 
 // 前回の問題文の数字
-let prevNum1 = null;
-let prevNum2 = null;
+let prevNum1 = 0;
+let prevNum2 = 0;
 
 // ボタンの数を取得
 const buttons = document.querySelectorAll('.js-checkAnswer');
 
 // 押したボタンの数字を定義
-let buttonText = null;
+let buttonText = 0;
 
-// 音声ファイルの初期化
-const correctAudio = new Audio('../sounds/correct.mp3');
-const incorrectAudio = new Audio('../sounds/incorrect.mp3');
+// 解いた問題数
+let count = 1;
+
+// 正解した数
+let correctCount = 0;
+
+// 問題数を設定
+let total = 5;
+
+// 音声ファイルの設定
+const correctAudio = '../sounds/correct.mp3';
+const incorrectAudio = '../sounds/incorrect.mp3';
+const startAudio = '../sounds/start.mp3';
+
+// 音を再生する関数
+function playSound(src) {
+  const audio = new Audio(src);
+  audio.play();
+}
 
 // 2つの数字をランダムに生成
 function generateNumbers() {
@@ -23,7 +39,14 @@ function generateNumbers() {
 }
 // 問題文の表示
 function displayProblem() {
+  // スタート音を再生
+  playSound(startAudio);
+
+  // 2つの数字をランダムに生成
   generateNumbers();
+
+  // 問題数の表示
+  document.getElementById('js-total').textContent = `もんだいすう ${count} / ${total}`;
 
   // 前回と同じ問題、または答えがマイナスなら再生成
   while ((num1 === prevNum1 && num2 === prevNum2) || num1 - num2 < 0) {
@@ -50,19 +73,39 @@ buttons.forEach((button) => {
 function checkAnswer() {
   const correctAnswer = num1 - num2;
 
-  if (buttonText === correctAnswer) {
-    correctAudio.play();
+  if (buttonText === correctAnswer && count < total) {
+    playSound(correctAudio);
 
     document.getElementById('js-result').textContent = 'せいかい！よくできました 🎉';
     document.getElementById('js-next').classList.remove('display-none');
-  } else {
-    incorrectAudio.play();
+    correctCount++;
+  } else if (buttonText !== correctAnswer && count < total) {
+    playSound(incorrectAudio);
 
     document.getElementById('js-result').textContent = 'ざんねん 😢';
     document.getElementById('js-correct').textContent = 'せいかいは、';
     document.getElementById('js-correct-num').textContent = correctAnswer;
 
     document.getElementById('js-next').classList.remove('display-none');
+  } else if (buttonText === correctAnswer && count === total) {
+    playSound(correctAudio);
+    document.getElementById('js-result').textContent = 'せいかい！よくできました 🎉';
+    correctCount++;
+
+    // 最終点数表示
+    const totalPoint = (correctCount / total) * 100;
+    document.getElementById('js-final').textContent = ` あなたのてんすうは ${totalPoint} てん 🎉`;
+    document.getElementById('js-new').classList.remove('display-none');
+  } else if (buttonText !== correctAnswer && count === total) {
+    playSound(incorrectAudio);
+    document.getElementById('js-result').textContent = 'ざんねん 😢';
+    document.getElementById('js-correct').textContent = 'せいかいは、';
+    document.getElementById('js-correct-num').textContent = correctAnswer;
+
+    // 最終点数表示
+    const totalPoint = (correctCount / total) * 100;
+    document.getElementById('js-final').textContent = `あなたのてんすうは ${totalPoint} てん 🎉`;
+    document.getElementById('js-new').classList.remove('display-none');
   }
 
   // ボタンを押せなくする
@@ -78,6 +121,9 @@ function nextProblem() {
   document.getElementById('js-correct').textContent = '';
   document.getElementById('js-correct-num').textContent = '';
 
+  // 出題数のカウント
+  count++;
+
   buttons.forEach((button) => {
     button.classList.remove('answer-button');
   });
@@ -86,6 +132,25 @@ function nextProblem() {
   // buttons.forEach((e) => {
   //   e.classList.toggle('pointer-none');
   // });
+
+  displayProblem();
+}
+
+// 「もういちどボタン」を押したとき
+function newProblem() {
+  document.getElementById('js-result').textContent = '';
+  document.getElementById('js-new').classList.add('display-none');
+  document.getElementById('js-final').textContent = '';
+  document.getElementById('js-correct').textContent = '';
+  document.getElementById('js-correct-num').textContent = '';
+  correctCount = 0;
+
+  // 出題数のリセット
+  count = 1;
+
+  buttons.forEach((button) => {
+    button.classList.remove('answer-button');
+  });
 
   displayProblem();
 }
